af52dcd58c80cab302f41e343f93e8587d04dbb9
chrisbateup <39247779+chrisbateup@users.noreply.github.com>
2018-06-22 15:43:37 +1000

    Update index.js
    Update title

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Secureply" />
     <Navbar />
     <div>{children()}</div>
   </div>
